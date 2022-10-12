6ff542014a543483c95158493eb2e1192121a20e
sw c <fortheboost@naver.com>
2022-10-12 16:08:19 +0900

    '1012-1'

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -13,10 +13,56 @@ class Header extends Component{
         };
     }
     componentDidMount(){
-        var cookie_userid = cookie.load('userid')
+        var cookie_useremail = cookie.load('useremail')
         var cookie_usernm = cookie.load('username')
         var cookie_password = cookie.load('userpassword')
+
+        if(cookie_useremail != undefined){
+
+            const expires = new Date()
+            expires.setMinutes(expires.getMinutes() + 60)
+
+            cookie.save('useremail',cookie_useremail,{path : '/' , expires})
+            cookie.save('username',cookie_usernm, { path : '/' , expires })
+            cookie.save('userpassword',cookie_password, {path : '/' , expires})
+
+            $('.menulist').show()
+            $('.hd_top').show()
+        }else{
+            $('.menulist').hide()
+            $('.hd_top').hide()
+        }
     }
+        callSessionInfoApi = (type) =>{
+            axios.post('/Login?sessionConfirm',{
+                token1 : cookie.load('useremail')
+                ,token2 : cookie.load('username')
+            })
+            .then(response => {
+                this.setState({usernm : response.data.token2})
+            }) 
+            .catch( error => {
+                this.sweetalert()
+            });
+        }
+        sweetalert = (title, contents, icon, confirmButtonText) => {
+            Swal.fire({
+                title: title,
+                text: contents,
+                icon: icon,
+                confirmButtonText: confirmButtonText
+              })
+        }
+
+        logout = async e => {
+            cookie.remove('useremail',{path: '/'});
+            cookie.remove('username',{path : '/'});
+            cookie.remove('userpassword', { path: '/'});
+            window.location.href = '/login';
+        }
+
+
+    
 
     
 
@@ -31,3 +77,5 @@ class Header extends Component{
     }
 
 }
+
+export default Header;
---
+++ frontend/src/userBoardDetail.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from "axios";
+import Swal from 'sweetalert2'
+import $ from 'jquery';
+
+class userBoardDetail extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            before_Boardid : props.match.params.bid,
+        }
+    }
+
+    componentDidMount(){
+        this.callBoardDetail()
+    }
+
+    callBoardDetail = async () =>{
+        axios.get('/board/getDetail',{
+            params: {bid : this.state.before_Boardid}
+        }).then(
+            response => {
+                try{
+                    alert(JSON.stringify(response));
+                    if(response.status =="200"){
+                        $('#title').val(response.data.title);
+                        $('#content').val(response.data.content);
+                    }
+                }catch(error){
+
+                }
+            }
+        )
+    }
+
+    render() {
+        return (
+            <section>
+
+                <h2>HELLOO</h2>
+                <input type="text" name="title" id="title"></input>
+                <input type="text" name="content" id="content"></input>
+            </section>
+        )
+    }
+
+}
+export default userBoardDetail;
